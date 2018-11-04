48e27835ae42bb87e216e75f72b496296f960b07
Rainie Gu <yg985@nyu.edu>
2018-11-03 21:56:22 -0400

    Update DC_python_function.py

--- DC_python_function.py
+++ DC_python_function.py
@@ -240,3 +240,242 @@ print(one_word)
 print(many_words)
 
 ##What makes **kwargs different is that it allows you to pass a variable number of keyword arguments to functions.
+# Define report_status
+def report_status(**kwargs):
+    """Print out the status of a movie character."""
+
+    print("\nBEGIN: REPORT\n")
+
+    # Print a formatted status report
+    for key, value in kwargs.items():
+        print(key + ": " + value)
+
+    print("\nEND REPORT")
+
+# First call to report_status()
+report_status(name="luke", affiliation="jedi", status="missing")
+
+# Second call to report_status()
+report_status(name="anakin", affiliation="sith lord", status="deceased")
+
+##bringing all together:
+#1 including a default argument
+# Define count_entries()
+def count_entries(df, col_name='lang'):
+    """Return a dictionary with counts of
+    occurrences as value for each key."""
+
+    # Initialize an empty dictionary: cols_count
+    cols_count = {}
+    
+    # Extract column from DataFrame: col
+    col = df[col_name]
+
+    # Iterate over the column in DataFrame
+    for entry in col:
+
+        # If entry is in cols_count, add 1
+        if entry in cols_count.keys():
+            cols_count[entry] += 1
+
+        # Else add the entry to cols_count, set the value to 1
+        else:
+            cols_count[entry] = 1
+
+    # Return the cols_count dictionary
+    return cols_count
+
+# Call count_entries(): result1
+result1 = count_entries(tweets_df, col_name='lang')
+
+# Call count_entries(): result2
+result2 = count_entries(tweets_df, col_name='source')
+
+# Print result1 and result2
+print(result1)
+print(result2)
+
+# Define count_entries()
+def count_entries(df, *args):
+    """Return a dictionary with counts of
+    occurrences as value for each key."""
+    
+    #Initialize an empty dictionary: cols_count
+    cols_count = {}
+    
+    # Iterate over column names in args
+    for col_name in args:
+    
+        # Extract column from DataFrame: col
+        col = df[col_name]
+    
+        # Iterate over the column in DataFrame
+        for entry in col:
+    
+            # If entry is in cols_count, add 1
+            if entry in cols_count.keys():
+                cols_count[entry] += 1
+    
+            # Else add the entry to cols_count, set the value to 1
+            else:
+                cols_count[entry] = 1
+
+    # Return the cols_count dictionary
+    return cols_count
+
+# Call count_entries(): result1
+result1 = count_entries(tweets_df, 'lang')
+
+# Call count_entries(): result2
+result2 = count_entries(tweets_df, 'lang', 'source')
+
+# Print result1 and result2
+print(result1)
+print(result2)
+
+#### lambda
+#1 Define echo_word as a lambda function: echo_word
+echo_word = lambda echo,word1:echo*word1
+
+# Call echo_word: result
+result = echo_word(5,'hey')
+
+# Print result
+print(result)
+
+#2 map :map() applies a function over an object, such as a list
+# Create a list of strings: spells
+spells = ["protego", "accio", "expecto patronum", "legilimens"]
+
+# Use map() to apply a lambda function over spells: shout_spells
+shout_spells = map(lambda a:a+'!!!', spells)
+# type(shout_spells) : map
+
+# Convert shout_spells to a list: shout_spells_list
+shout_spells_list = list(shout_spells)
+
+# Convert shout_spells into a list and print it
+print(shout_spells_list)
+
+#3 filter() offers a way to filter out elements from a list that don't satisfy certain criteria.
+# Create a list of strings: fellowship
+fellowship = ['frodo', 'samwise', 'merry', 'pippin', 'aragorn', 'boromir', 'legolas', 'gimli', 'gandalf']
+
+# Use filter() to apply a lambda function over fellowship: result
+result = filter(lambda a:len(a)>6, fellowship)
+
+# Convert result to a list: result_list
+result_list=list(result)
+
+# Convert result into a list and print it
+print(result_list)
+
+# 4: The reduce() function is useful for performing some computation on a list and, unlike map() and filter(), returns a single value as a result. 
+# To use reduce(), you must import it from the functools module.
+# Import reduce from functools
+from functools import reduce
+
+# Create a list of strings: stark
+stark = ['robb', 'sansa', 'arya', 'brandon', 'rickon']
+
+# Use reduce() to apply a lambda function over stark: result
+result = reduce(lambda item1,item2: item1+item2, stark)
+
+# Print the result
+print(result)
+
+#### Error handling with try-except
+# Define shout_echo
+def shout_echo(word1, echo=1):
+    """Concatenate echo copies of word1 and three
+    exclamation marks at the end of the string."""
+
+    # Initialize empty strings: echo_word, shout_words
+    echo_word = ''
+    shout_words = ''
+
+    # Add exception handling with try-except
+    try:
+        # Concatenate echo copies of word1 using *: echo_word
+        echo_word = word1 * echo
+
+        # Concatenate '!!!' to echo_word: shout_words
+        shout_words = echo_word + '!!!'
+    except:
+        # Print error message
+        print("word1 must be a string and echo must be an integer.")
+
+    # Return shout_words
+    return shout_words
+
+# Call shout_echo
+shout_echo("particle", echo="accelerator")
+
+#Another way to raise an error is by using raise. In this exercise, you will add a raise statement to the shout_echo() function you defined before to raise an error message when the value supplied by the user to the echo argument is less than 0.
+# Define shout_echo
+def shout_echo(word1, echo=1):
+    """Concatenate echo copies of word1 and three
+    exclamation marks at the end of the string."""
+
+    # Raise an error with raise
+    if echo < 0:
+        raise ValueError('echo must be greater than 0')
+
+    # Concatenate echo copies of word1 using *: echo_word
+    echo_word = word1 * echo
+
+    # Concatenate '!!!' to echo_word: shout_word
+    shout_word = echo_word + '!!!'
+
+    # Return shout_word
+    return shout_word
+
+# Call shout_echo
+shout_echo("particle", echo=5)
+
+# n the filter() call, pass a lambda function and the sequence of tweets as strings, tweets_df['text']. The lambda function should check if the first 2 characters in a tweet x are 'RT'. 
+# Select retweets from the Twitter DataFrame: result
+result = filter(lambda x: x[0:2] == 'RT', tweets_df['text'])
+
+# Create list from filter object result: res_list
+res_list = list(result)
+
+# Print all retweets in res_list
+for tweet in res_list:
+    print(tweet)
+
+# Define count_entries()
+def count_entries(df, col_name='lang'):
+    """Return a dictionary with counts of
+    occurrences as value for each key."""
+    
+    # Raise a ValueError if col_name is NOT in DataFrame
+    if col_name not in df.columns:
+        raise ValueError('The DataFrame does not have a ' + col_name + ' column.')
+
+    # Initialize an empty dictionary: cols_count
+    cols_count = {}
+    
+    # Extract column from DataFrame: col
+    col = df[col_name]
+    
+    # Iterate over the column in DataFrame
+    for entry in col:
+
+        # If entry is in cols_count, add 1
+        if entry in cols_count.keys():
+            cols_count[entry] += 1
+            # Else add the entry to cols_count, set the value to 1
+        else:
+            cols_count[entry] = 1
+        
+        # Return the cols_count dictionary
+    return cols_count
+
+# Call count_entries(): result1
+result1=count_entries(tweets_df,col_name='lang')
+
+# Print result1
+print(result1)
+
+
